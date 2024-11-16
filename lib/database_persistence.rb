e58bbff1bc7d47a787089d474f2f89bdec848198
Esther Ho <esther.ho.ucd@gmail.com>
2024-11-16 12:36:47 -0800

    Add feature to find a user based on their username
    - Filter the `users` table based on the given username
    - Return the relevant tuple containing the user information

--- lib/database_persistence.rb
+++ lib/database_persistence.rb
@@ -9,4 +9,18 @@ class DatabasePersistence
   def disconnect
     @db.close
   end
+
+  # Find a user from the `users` table based on the given username
+  def find_user(username)
+    sql = "SELECT * FROM users WHERE username = $1"
+    result = query(sql, username)
+    result.first
+  end
+
+  private
+
+  def query(statement, *params)
+    @logger&.info "#{statement}: #{params}"
+    @db.exec_params(statement, params)
+  end
 end
