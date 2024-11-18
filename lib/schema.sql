aed293c7ced91e1e5e4c61711b863d0d9f7f63ab
Esther Ho <esther.ho.ucd@gmail.com>
2024-11-18 10:56:36 -0800

    feat: update schema to include a new `vaults` table
    This commit adds a new `vaults` table that includes the columns:
    - id (primary key, auto-increment)
    - name (100 character limit, not null)
    - created_at (not null, now() by default)
    - updated_at (not null, now() by default)
    - user_id (not null, foreign key that references `id` in `users` table)
    
    This change allows users to create vaults that are associated with
    password entries.

--- lib/schema.sql
+++ lib/schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS users (
   updated_at timestamp NOT NULL DEFAULT now(),
   UNIQUE (username, password_hash)
 );
+
+CREATE TABLE IF NOT EXISTS vaults (
+  id serial PRIMARY KEY,
+  name varchar(100) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT now(),
+  updated_at timestamp NOT NULL DEFAULT now(),
+  user_id integer NOT NULL REFERENCES users (id) ON DELETE CASCADE
+);
