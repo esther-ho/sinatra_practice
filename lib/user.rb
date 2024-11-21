ef624c639a36b683d14a50336c62df6edda40f0a
Esther Ho <esther.ho.ucd@gmail.com>
2024-11-20 20:16:03 -0800

    refactor: move user instance methods to class methods
    This commit moves the `#add_user` and `#find_user` methods into class
    methods. It also invokes the `DatabaseAccessor::query` method within
    each method to add or retrieve the user entry.

--- lib/user.rb
+++ lib/user.rb
@@ -1,14 +1,14 @@
 class User
   # Add a new user to the `users` table
-  def add_user(username, password)
+  def self.add_user(username, password)
     sql = "INSERT INTO users (username, password_hash) VALUES ($1, $2)"
-    query(sql, username, password)
+    DatabaseAccessor.query(sql, username, password)
   end
 
   # Find a user from the `users` table based on the given username
-  def find_user(username)
+  def self.find_user(username)
     sql = "SELECT * FROM users WHERE username = $1"
-    result = query(sql, username)
+    result = DatabaseAccessor.query(sql, username)
     result.first
   end
 end
