5cf367034a4bd1dd32c4e7097bc57f25cbda5a33
Esther Ho <esther.ho.ucd@gmail.com>
2024-11-20 20:21:28 -0800

    style: rename `::add_user` and `::find_user`
    This commit renames `::add_user` to `::add` and `::find_user` to
    `::find_by_username` to eliminate redundancy and improve clarity.

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,12 @@
 class User
   # Add a new user to the `users` table
-  def self.add_user(username, password)
+  def self.add(username, password)
     sql = "INSERT INTO users (username, password_hash) VALUES ($1, $2)"
     DatabaseAccessor.query(sql, username, password)
   end
 
   # Find a user from the `users` table based on the given username
-  def self.find_user(username)
+  def self.find_by_username(username)
     sql = "SELECT * FROM users WHERE username = $1"
     result = DatabaseAccessor.query(sql, username)
     result.first
