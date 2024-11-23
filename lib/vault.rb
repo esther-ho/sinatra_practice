b27dd322ecaaad0c0f06897dfc6aece8744086af
Esther Ho <esther.ho.ucd@gmail.com>
2024-11-23 15:55:18 -0800

    refactor: move user instance methods to class methods
    This commit moves the `#add_vault` and `#find_vault` methods into class
    methods. It also invokes the `DatabaseAccessor::query` method within
    each method to add or retrieve the vault entry.
    
    It also renames `#add_vault` to `::add` and `#find_vault` to
    `::find_by_vault_name` to eliminate redundancy and improve clarity.

--- lib/vault.rb
+++ lib/vault.rb
@@ -1,14 +1,14 @@
 class Vault
   # Add a vault associated with a user based on the given user id
-  def add_vault(user_id, vault_name)
+  def self.add(user_id, vault_name)
     sql = "INSERT INTO vaults (user_id, name) VALUES ($1, $2)"
-    query(sql, user_id, vault_name)
+    DatabaseAccessor.query(sql, user_id, vault_name)
   end
 
   # Find a vault with the given vault name of a specific user
-  def find_vault(user_id, vault_name)
+  def self.find_by_vault_name(user_id, vault_name)
     sql = "SELECT * FROM vaults WHERE user_id = $1 AND name ILIKE $2"
-    result = query(sql, user_id, vault_name)
+    result = DatabaseAccessor.query(sql, user_id, vault_name)
     result.first
   end
 end
