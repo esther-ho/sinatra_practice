e5f3db49f9167679b1133d08d1bca350054e3257
Esther Ho <esther.ho.ucd@gmail.com>
2024-11-29 14:09:31 -0800

    feat: add error types and messages to `Error` object
    This commit adds a method that modifies the `@errors` hash in the
    `Error` instance by defining error types as hash keys, and an array of
    error messages as hash values. It also adds a method that returns a copy
    of the `@errors` hash.
    
    It updates the test file to verify that the error type and message are
    added.

--- lib/error.rb
+++ lib/error.rb
@@ -2,4 +2,22 @@ class Error
   def initialize
     @errors = {}
   end
+
+  # Assign the `error_type` as a key e.g. `:invalid_username`
+  # Add messages related to an error into the array referenced by the key
+  def add(error_type, message)
+    errors[error_type] ||= []
+    errors[error_type] << message
+    self
+  end
+
+  # Return a hash where keys are the error type,
+  # and values are the array of messages
+  def details
+    errors.clone.freeze
+  end
+
+  private
+
+  attr_reader :errors
 end
--- test/error_test.rb
+++ test/error_test.rb
@@ -3,9 +3,22 @@ require "minitest/autorun"
 require_relative "../lib/error"
 
 class ErrorTest < Minitest::Test
+  def setup
+    @errors = Error.new
+    @errors.add(:invalid_username, "Username is already taken.")
+    @errors.add(:invalid_username,
+                "Username must only contain alphanumeric characters.")
+  end
+
   def test_raises_error_initialize_with_argument
     assert_raises ArgumentError do
       Error.new('Test error.')
     end
+  end
+
+  def test_add_error
+    messages = ["Username is already taken.",
+                "Username must only contain alphanumeric characters."]
+    assert_equal ({ invalid_username: messages }), @errors.details
   end
 end
