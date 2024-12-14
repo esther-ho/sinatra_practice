22b77aaf72b5022cc0f5f6ba977a7c0b77302275
Esther Ho <esther.ho.ucd@gmail.com>
2024-12-13 20:26:32 -0800

    feat: add `credentials` table
    This commit updates the schema to add a `credentials` table that
    includes the columns:
    - id (primary key, auto-increment)
    - user_id (not null, foreign key that references `id` in the `users`
    table)
    - name (not null)
    - username (not null)
    - encrypted password
    - iv
    - notes
    
    There is a UNIQUE constraint for (`name`, `username`) such that the
    name-username combination cannot be repeated. This change allows users
    to store credentials to the services they are using. Encrypted password
    and iv are not required since some services may involve single sign-on.

--- lib/schema.sql
+++ lib/schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE IF NOT EXISTS users (
   updated_at timestamp NOT NULL DEFAULT now(),
   UNIQUE (username, password_hash)
 );
+
+CREATE TABLE IF NOT EXISTS credentials (
+  id serial PRIMARY KEY,
+  user_id integer NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+  name text NOT NULL,
+  username text NOT NULL,
+  encrypted_password text,
+  iv text,
+  notes text,
+  UNIQUE(name, username)
+);
