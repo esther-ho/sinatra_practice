f6211a2a9448a7e47facbd8e50c479cc0cd2967b
Esther Ho <esther.ho.ucd@gmail.com>
2024-12-16 14:51:55 -0800

    test-fix: update tests for `DatabaseAccessor` class
    This commit removes the `Vault` method invocations in the tests
    verifying the behavior of the `DatabaseAccessor` class.

--- test/database_accessor_test.rb
+++ test/database_accessor_test.rb
@@ -4,7 +4,6 @@ require "minitest/autorun"
 
 require_relative "../lib/database_accessor"
 require_relative "../lib/user"
-require_relative "../lib/vault"
 
 class DatabaseAccessorTest < Minitest::Test
   def setup
@@ -18,32 +17,21 @@ class DatabaseAccessorTest < Minitest::Test
 
   def test_delete_all_data
     User.add("admin", "123")
-    Vault.add("1", "My Vault")
 
     user1 = User.find_by_username("admin")
-    vault1 = Vault.find_by_vault_name("1", "My vault")
 
     assert user1
-    assert_equal "1", user1.id
-
-    assert vault1
-    assert_equal "1", vault1.id
+    assert_equal 1, user1.id
 
     DatabaseAccessor.reset
 
     assert_nil User.find_by_username("admin")
-    assert_nil Vault.find_by_vault_name("1", "My Vault")
 
     User.add("developer", "123")
-    Vault.add("1", "My Vault")
 
     user2 = User.find_by_username("developer")
-    vault2 = Vault.find_by_vault_name(1, "My Vault")
 
     assert user2
-    assert_equal "1", user2.id
-
-    assert vault2
-    assert_equal "1", vault2.id
+    assert_equal 1, user2.id
   end
 end
